e483e7541d8fd888a9824bca05364f111eb084dc
Nicolas <nicolasecrespo@gmail.com>
2024-01-24 21:42:12 -0500

    changes to fish prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,10 +1,9 @@
 function fish_prompt
-    if test $PWD = $HOME
-        echo -n (set_color green)"~"
-    else
-        echo -n (set_color green)(prompt_pwd)(set_color blue)(fish_git_prompt)
-        set_color normal
-    end
-    echo -n (set_color green)"  "
+    # if test $PWD = $HOME
+    #     echo -n (set_color green)"~ "
+    # else
+    # end
+    echo -n (set_color green)(prompt_pwd)
+    echo -n (set_color blue)"  "
     set_color normal
 end
