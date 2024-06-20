bab7ebae67b1c20c1f32f6d251f05dc136bc93a1
Nicolas <nicolasecrespo@gmail.com>
2024-06-20 03:38:06 -0400

    remove prompt temporarily

--- functions/fish_prompt.fish
+++
@@ -1,9 +0,0 @@
-function fish_prompt
-    # if test $PWD = $HOME
-    #     echo -n (set_color green)"~ "
-    # else
-    # end
-    echo -n (set_color 89ca78)(prompt_pwd)
-    echo -n (set_color 61afef)"  "
-    set_color normal
-end
