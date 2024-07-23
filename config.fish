8fcf0003917e6a458a075027860eeaf9f0523e2c
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-07-22 23:23:55 -0400

    clean up fish

--- config.fish
+++ config.fish
@@ -12,7 +12,6 @@ set -Ux fish_user_paths $fish_user_paths /home/nicolas/julia-1.8.1/bin /home/nic
 set -g async_prompt_functions _pure_prompt_git
 
 set -Ux EDITOR /usr/bin/nvim # set correct editor
-# set -Ux JAVA_HOME /usr/lib/jvm/jdk-17-oracle-x64
 set -gx EDITOR nvim # set correct editor
 set -Ux FZF_DEFAULT_OPTS "--height 100% --no-preview "
 set -x DISPLAY :0 # fix vscode
@@ -21,10 +20,9 @@ eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv) # some brew stuff
 set fish_prompt_pwd_dir_length 0 # don't abbreviate paths in prompt
 set -Ux FZF_FIND_FILE_COMMAND "find . -type d -name .git -prune -o -type f -print"
 set -Ux FZF_OPEN_COMMAND "fd --type f --exclude .git --hidden"
-set -Ux TERM xterm-256color
 
 zoxide init --cmd j fish | source # zoxide
-theme_tokyonight storm
+theme_tokyonight night
 
 # # #2bbac5
 set pure_color_primary 34e2e2
@@ -35,10 +33,8 @@ set pure_color_info 4e9a06
 # # #4e9a06
 set pure_color_mute 4e9a06
 
-# set --global hydro_symbol_prompt '→'
 set --global hydro_symbol_git_dirty '*'
 set --global fish_prompt_pwd_dir_length 100
-# set --global hydro_color_pwd
 
 set pure_color_warning green
 
@@ -103,19 +99,6 @@ function g:
     git push
 end
 
-# # add ssh-key if shell is opened on startup, not in random shell instances
-# set parent_process (ps -o ppid= -p $fish_pid)
-# set parent_process (string trim $parent_process)
-# set parent_process (ps -o comm -p $parent_process | tail -n +2 | grep -v '^$')
-# set -l prev_command (history | head -n 1 | string trim)
-
-# if test "$parent_process" = su
-#     if not set -q SSH_AGENT_PID
-#         skey >/dev/null 2>&1
-#         echo "ssh-key added!"
-#     end
-# end
-
 # Check if SSH agent is already running
 if not set -q SSH_AUTH_SOCK
     skey >/dev/null 2>&1
@@ -164,53 +147,35 @@ alias mv 'mv -i'
 alias rm 'trash -v'
 alias mkdir 'mkdir -p'
 alias psa 'ps auxf'
-alias ping 'ping -c 10'
 alias .. 'cd ..'
 alias .. 'cd ..'
 alias ... 'cd ../..'
 alias .... 'cd ../../..'
-alias ..... 'cd ../../../..'
 alias ls 'eza --icons=always --group-directories-first' # add colors and file type extensions
 alias la 'ls -Alh --group-directories-first' # show hidden files
 alias l 'ls -l --group-directories-first --git -a'
 alias lt 'ls --tree' # tree
 alias p "ps aux | grep " # Search running processes
 alias f "find . | grep " # Search files in the current folder
-alias cs 'cd ~/grade-12/cs/'
-alias csa 'cd ~/grade-12/csa/'
-alias exp 'wopen .' # wsl specific, open explorer in cwd
+alias exp 'wsl-open .' # wsl specific, open explorer in cwd
 alias shutdown '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -c wsl --shutdown'
 alias nala 'sudo nala'
-alias wopen wsl-open
-alias su 'su -'
 alias csv csvlens
-alias rmm dos2unix
-alias nn /usr/bin/nvim
 alias win "/mnt/c/Program\ Files/PowerShell/7/pwsh.exe"
 alias powershell.exe "/mnt/c/Program\ Files/PowerShell/7/pwsh.exe"
 alias cmd.exe "/mnt/c/Windows/System32/cmd.exe"
-alias bd 'cd -'
-alias pbcopy 'xsel --input --clipboard'
-alias pbpaste 'xsel --output --clipboard'
-alias live 'live-server --browser=/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe&'
-# "C:\Program Files\WindowsApps\Microsoft.PowerShell_7.4.1.0_x64__8wekyb3d8bbwe\pwsh.exe"
 
 # ----------------------- #
 #     ABBREVIATIONS
 # ----------------------- #
 
-abbr cat bat
-abbr cls clear
 abbr lg lazygit
-abbr gg lazygit
 abbr n. 'nvim .'
-abbr img 'wezterm imgcat'
 abbr v vim
 abbr g git
 abbr q exit
 abbr sl ls
 abbr dc cd
-abbr wther 'curl wttr.in'
 abbr weather 'curl wttr.in'
 abbr ra ranger
 abbr n nvim
@@ -219,9 +184,6 @@ abbr ff fastfetch
 # ----------------------- #
 #       KEYBINDINGS
 # ----------------------- #
-
-# Bind Ctrl + Z to the select_job_with_fzf function
-# bind -M insert \cz "select_job_with_fzf;commandline -f repaint"
 
 bind -M insert \cz "fg; commandline -f repaint"
 bind -M insert \e\[13\;5u accept-autosuggestion # control-enter for accept-autosuggestion
@@ -233,8 +195,6 @@ bind -M insert \n "__zoxide_zi; commandline -f kill-whole-line; commandline -f r
 bind -M insert \e\x7F kill-whole-line repaint # use <M-BS> for clearing line
 bind -M insert \cE "e; commandline -f repaint"
 bind -M insert \cS "prepend_command sudo"
-# bind \cg 'git diff; commandline -f repaint'
-# bind -M insert \cc kill-whole-line repaint
 
 ## >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
