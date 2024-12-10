0634f5c1e3f6f6869a803ef421d8fe8834584cbb
Nicolas Crespo <nicolasecrespo@gmail.com>
2024-12-09 19:32:47 -0800

    add focus alias

--- config.fish
+++ config.fish
@@ -194,6 +194,7 @@ alias win "/mnt/c/Program\ Files/PowerShell/7/pwsh.exe"
 alias wintop 'win --c "btop"'
 alias powershell.exe "/mnt/c/Program\ Files/PowerShell/7/pwsh.exe"
 alias cmd.exe "/mnt/c/Windows/System32/cmd.exe"
+alias focus 'telnet towel.blinkenlights.nl'
 # alias vim nvim
 
 # ----------------------- #
