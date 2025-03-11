15c9b6ab86754ecc82bc50d84070e5aaee19cbed
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-03-10 22:29:32 -0700

    feat: add gh-create function

--- config.fish
+++ config.fish
@@ -174,6 +174,19 @@ function notify-send
     /mnt/c/Users/nicol/OneDrive/Desktop/Applications/wsl-notify-send.exe --category $WSL_DISTRO_NAME "$argv"
 end
 
+# creates a git repo from the current directory,
+# optional parameter for the name of the repo
+function gh-create
+    set repo_name $argv[1]
+    if test -n "$repo_name"
+        gh repo create $repo_name --private --source=. --remote=origin
+    else
+        gh repo create --private --source=. --remote=origin
+    end
+    git push -u --all
+    gh browse
+end
+
 #
 # ----------------------- #
 #        ALIASES
