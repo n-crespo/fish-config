d7effb44177d09fbef99c30614bd31c6d975a4f1
Nicolas Crespo <nicolasecrespo@gmail.com>
2025-09-07 17:08:36 -0400

    refactor: improve fzf_jump logic

--- functions/fzf_jump.fish
+++ functions/fzf_jump.fish
@@ -1,9 +1,13 @@
 function fzf_jump
-    set -l selected_dir (zoxide query -l | string replace -r "^$HOME" "~" | fzf)
+    set -l selected_dir (zoxide query -l | string replace -r "^$HOME/" "" | fzf)
     if test -n "$selected_dir"
         set selected_dir (string replace -r "^~" "$HOME" "$selected_dir")
-        cd "$selected_dir"
-        if set -q _flag_nvim
+        if string match -q "/*" -- $selected_dir
+            cd "$selected_dir"
+        else
+            cd "$HOME/$selected_dir"
+        end
+        if contains -- --edit $argv
             nvim -c "lua require('persistence').load()"
         end
     end
